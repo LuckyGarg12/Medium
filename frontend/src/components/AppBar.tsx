d50dae42244849dd1208fca645e32bb31402aee8
LuckyGarg12 <luckygarg452@gmail.com>
2025-07-05 10:27:23 +0530

    Added Sign Out button

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,9 +1,10 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { AvatarAppBar } from "./Avatar"
 
 
 export const AppBar = () => {
     const username = "Lucky";
+    const navigate = useNavigate();
 
     return(
         <div className="flex justify-between px-5 py-3 border-b-1 border-slate-300 w-full bg-white">
@@ -13,12 +14,18 @@ export const AppBar = () => {
                 </div>
             </Link>
 
-            <div>
+            <div className="flex justify-center">
                 <Link to="/publish">
-                    <button type="button" className="mr-4 text-white bg-green-500 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center">
-                        New
+                    <button type="button" className="mr-4 text-white bg-green-500 hover:bg-green-700 active:outline-none active:ring-2 active:ring-green-800 font-medium rounded-full text-sm px-5 py-2.5 text-center">
+                        Publish
                     </button>
                 </Link>
+                <button className="mr-4 text-white bg-gray-700 hover:bg-gray-900 active:outline-none active:ring-2 active:ring-gray-800 font-medium rounded-full text-sm px-5 py-2.5 text-center" onClick={() => {
+                    localStorage.removeItem("token")
+                    navigate("/")
+                }}>
+                    Sign Out
+                </button>
                 <AvatarAppBar name={username} />
             </div>
         </div>
--- frontend/src/pages/Publish.tsx
+++ frontend/src/pages/Publish.tsx
@@ -30,11 +30,18 @@ export const Publish = () => {
     return (
         <div>
             <AppBar />
-            <div className="px-10 pt-20 flex justify-center">
-                <div className="mb-6 px-5 max-w-3xl min-w-screen md:min-w-3xl flex justify-center flex-col">
-                    <input ref={title} type="text" id="title" placeholder="Title" className="w-full max-w-2xl text-lg p-4 outline-0 text-gray-900 border-1 rounded-lg border-slate-400 focus:border-slate-600" />    
-                    <textarea ref={content} id="message" rows={8} className="block max-w-2xl outline-0 mt-5 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:border-slate-600 " placeholder="Write your Content here..."></textarea>
-                    <div className="flex justify-center max-w-2xl mt-5">
+            <div className="px-10 py-30 flex justify-center">
+                <div className="flex justify-center flex-col px-5 max-w-3xl min-w-screen md:min-w-3xl">
+                    <div className="text-6xl font-bold text-center w-full mb-10">
+                        Let the world know!
+                    </div>
+                    <div className="flex justify-center">
+                        <input ref={title} type="text" id="title" placeholder="Title" className="w-full max-w-2xl text-lg p-4 outline-0 text-gray-900 border-1 rounded-lg border-slate-400 focus:border-slate-600" />    
+                    </div>
+                    <div className="flex justify-center">
+                        <textarea ref={content} id="message" rows={8} className="block max-w-2xl outline-0 mt-5 p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:border-slate-600 " placeholder="Write your Content here..."></textarea>
+                    </div>
+                    <div className="flex justify-center mt-5">
                         <button type="submit" onClick={publish_button} className="w-30 px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg active:ring-2 active:ring-blue-200 hover:bg-blue-800">
                             Publish Post
                         </button> 
